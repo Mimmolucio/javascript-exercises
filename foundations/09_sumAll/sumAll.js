1f70de9363a4176f1510793a36ef0860cb309d9e
Mimmolucio <domenico.lp@gmail.com>
2026-02-13 14:19:11 +0100

    finished foundations 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 < 0 || num1 < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR"
+    }
+
+    let low = num1;
+    let high = num2;
+    
+    if(num1 > num2) {
+        low = num2;
+        high = num1;
+    }
+
+    let sum = 0;
+
+    for(let i = low; i <= high; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
